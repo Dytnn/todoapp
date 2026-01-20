344d5138c1d4899d2117195e02d3a853eb8cd7e6
Dytnn <1111bepresent@gmail.com>
2026-01-20 16:16:08 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/todoapp/' : '/',
-}))
+  // Vercel 배포 시에는 '/', GitHub Pages 배포 시에는 '/todoapp/' 사용
+  base: process.env.VERCEL === '1' || !process.env.GITHUB_ACTIONS ? '/' : '/todoapp/',
+})
